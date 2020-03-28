7ad2e1761da3726119c449352609112977501cf5
iamnabink <nabrajkhadka43@gmail.com>
2020-03-28 22:37:17 +0545

    updated some apis

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -84,16 +84,15 @@ class AuthController extends Controller
                 $tokenResult = $user->createToken('Personal Access Token');
                 if ($request->remember_me){
                     $token = $tokenResult->token;
-                    $token->expires_at = Carbon::now()->addWeeks(1);
+//                    $token->expires_at = Carbon::now()->addWeeks(1);
                     $token->save();
                 }
                 $data = array();
                 $data['users'] = $user;
                 $tokenArray = array('access_token' => $tokenResult->accessToken,
                     'token_type' => 'Bearer',
-                    'expires_at' => Carbon::parse(
-                        $tokenResult->token->expires_at
-                    )->toDateTimeString());
+//                    'expires_at' => Carbon::parse($tokenResult->token->expires_at)->toDateTimeString()
+                );
                 $data['token'] = $tokenArray;
                 return response()->json([
                     'status' => true,
